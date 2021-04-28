a86ef92676d5ab5eaec0b441201d11b902802a8e
T-Bak7 <83053321+T-Bak7@users.noreply.github.com>
2021-04-28 14:45:57 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+music.setVolume(100)
 let PLAYER1 = sprites.create(img`
     f f f f f f f f f f f f f f f . . . 
     f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
@@ -37,3 +38,27 @@ let PLAYER2 = sprites.create(img`
 tiles.setTilemap(tilemap`level1`)
 PLAYER1.setPosition(10, 10)
 PLAYER2.setPosition(150, 110)
+forever(function () {
+    music.playMelody("G F G A - F E D ", 360)
+    music.playMelody("E D E G - E D C ", 360)
+    music.playMelody("E D E D E G - E ", 360)
+    music.playMelody("G G G G - - - - ", 360)
+})
+forever(function () {
+    music.playMelody("G F G A - F E D ", 360)
+    music.playMelody("E D E G - E D C ", 360)
+    music.playMelody("E D E D E G - E ", 360)
+    music.playMelody("C C C C - - - - ", 360)
+})
+forever(function () {
+    music.playMelody("G F G A - F E D ", 360)
+    music.playMelody("E D E G - E D C ", 360)
+    music.playMelody("E D E D E G - E ", 360)
+    music.playMelody("E E E E - - - - ", 360)
+})
+forever(function () {
+    music.playMelody("G F G A - F E D ", 360)
+    music.playMelody("E D E G - E D C ", 360)
+    music.playMelody("E D E D E G - E ", 360)
+    music.playMelody("C5 C5 C5 C5 - - - - ", 360)
+})
