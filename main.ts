5d6059306920fa6aa75c02284e8ebbd332c0aa9c
T-Bak7 <83053321+T-Bak7@users.noreply.github.com>
2021-04-28 11:24:22 +1000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,39 @@
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . . . 
-    . f f f f f f f f f f f f f f . . . 
-    . f 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    . f 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    . f 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    . f 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    . f 8 8 8 8 8 8 8 f f f f f f f f f 
-    . f 8 8 8 8 8 8 8 f 8 8 8 8 8 8 8 f 
-    . f 8 8 8 8 8 8 8 f 8 8 8 8 8 8 8 f 
-    . f 8 8 8 8 8 8 8 f f f f f f f f f 
-    . f 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    . f 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    . f 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    . f 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    . f f f f f f f f f f f f f f . . . 
-    . . . . . . . . . . . . . . . . . . 
+let PLAYER1 = sprites.create(img`
+    f f f f f f f f f f f f f f f . . . 
+    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
+    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
+    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
+    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
+    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
+    f 8 8 8 8 8 8 8 8 f f f f f f f f f 
+    f 8 8 8 8 8 8 8 8 f 8 8 8 8 8 8 8 f 
+    f 8 8 8 8 8 8 8 8 f 8 8 8 8 8 8 8 f 
+    f 8 8 8 8 8 8 8 8 f f f f f f f f f 
+    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
+    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
+    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
+    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
+    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
+    f f f f f f f f f f f f f f f . . . 
     `, SpriteKind.Player)
+let PLAYER2 = sprites.create(img`
+    f f f f f f f f f f f f f f f . . . 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . . . 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . . . 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . . . 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . . . 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . . . 
+    f 2 2 2 2 2 2 2 2 f f f f f f f f f 
+    f 2 2 2 2 2 2 2 2 f 2 2 2 2 2 2 2 f 
+    f 2 2 2 2 2 2 2 2 f 2 2 2 2 2 2 2 f 
+    f 2 2 2 2 2 2 2 2 f f f f f f f f f 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . . . 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . . . 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . . . 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . . . 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . . . 
+    f f f f f f f f f f f f f f f . . . 
+    `, SpriteKind.Player)
+tiles.setTilemap(tilemap`level1`)
+PLAYER1.setPosition(10, 10)
+PLAYER2.setPosition(150, 110)
---
+++ tilemap.g.ts
@@ -0,0 +1,31 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`0a0008000101010101010501010101010101010101010101010105010501010105040101010101010103010101010201010101010101010501010101010101030101010105050401050101050101010101010101`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass1,sprites.castle.tilePath4,sprites.castle.tilePath6,sprites.castle.tilePath2,sprites.castle.tileGrass2], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
