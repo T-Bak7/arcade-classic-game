16d5397aaab27af615a86a02364d634ee965c5ab
T-Bak7 <83053321+T-Bak7@users.noreply.github.com>
2021-04-30 14:06:35 +1000

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
-music.setVolume(100)
+music.setVolume(255)
 let PLAYER1 = sprites.create(img`
-    f f f f f f f f f f f f f f f . . . 
-    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    f 8 8 8 8 8 8 8 8 f f f f f f f f f 
-    f 8 8 8 8 8 8 8 8 f 8 8 8 8 8 8 8 f 
-    f 8 8 8 8 8 8 8 8 f 8 8 8 8 8 8 8 f 
-    f 8 8 8 8 8 8 8 8 f f f f f f f f f 
-    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    f 8 8 8 8 8 8 8 8 8 8 8 8 8 f . . . 
-    f f f f f f f f f f f f f f f . . . 
+    f f f f f f f f f f f f f f . . . . 
+    e e e e e e e e e e e e e e . . . . 
+    e e e e e e c e e e c c e e . . . . 
+    e e c e e e e e e e e e e e . . . . 
+    e e e e f f f f f f f e e e . . . . 
+    e e e f c f f f f c f f e e . . . . 
+    e e e f f f f f f f f f e e . . . . 
+    e e e f f f f f 1 f f f f f f f f . 
+    e e e f f f f f 1 f f f f c f f f . 
+    e e e f f f f f f f f f e e . . . . 
+    e e e f f f f f f f f f e c . . . . 
+    c e e e f f f f f f f e e e . . . . 
+    e e e e e e e e e e e e e e . . . . 
+    e e c e e e c e e e e c e e . . . . 
+    e e e e e e e e e e e e e e . . . . 
+    f f f f f f f f f f f f f f . . . . 
     `, SpriteKind.Player)
 let PLAYER2 = sprites.create(img`
     f f f f f f f f f f f f f f f . . . 
@@ -35,16 +35,11 @@ let PLAYER2 = sprites.create(img`
     f 2 2 2 2 2 2 2 2 2 2 2 2 2 f . . . 
     f f f f f f f f f f f f f f f . . . 
     `, SpriteKind.Player)
-tiles.placeOnTile(PLAYER1, tiles.getTileLocation(2, 8))
-tiles.placeOnTile(PLAYER2, tiles.getTileLocation(8, 6))
 tiles.setTilemap(tilemap`level1`)
-PLAYER1.setStayInScreen(true)
-PLAYER2.setStayInScreen(true)
-PLAYER1.setBounceOnWall(true)
-PLAYER2.setBounceOnWall(true)
+PLAYER1.setPosition(10, 10)
+PLAYER2.setPosition(146, 106)
 forever(function () {
-    PLAYER1.setVelocity(50, 50)
-    PLAYER2.setVelocity(50, 50)
+	
 })
 forever(function () {
 	
