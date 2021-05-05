7cd8aa865a68a89026f85217d0f0d1bd918c8a1d
T-Bak7 <83053321+T-Bak7@users.noreply.github.com>
2021-05-05 14:33:58 +1000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+namespace SpriteKind {
+    export const projectile2 = SpriteKind.create()
+    export const Player2 = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player2, function (sprite, otherSprite) {
+    game.over(false, effects.blizzard)
     music.powerDown.play()
-    game.over(false)
 })
+info.onCountdownEnd(function () {
+    pause(2000)
+    game.reset()
+})
+sprites.onOverlap(SpriteKind.projectile2, SpriteKind.Player, function (sprite, otherSprite) {
+    game.over(false, effects.blizzard)
+    music.powerDown.play()
+})
+let projectile2: Sprite = null
 let projectile: Sprite = null
 music.setVolume(255)
 let PLAYER1 = sprites.create(img`
@@ -45,6 +58,7 @@ PLAYER1.setPosition(10, 10)
 PLAYER2.setPosition(146, 106)
 controller.moveSprite(PLAYER1)
 controller.player2.moveSprite(PLAYER2)
+info.startCountdown(30)
 forever(function () {
     if (controller.player1.isPressed(ControllerButton.Up)) {
         projectile = sprites.createProjectileFromSprite(img`
@@ -65,11 +79,31 @@ forever(function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, PLAYER1, 0, -100)
+        PLAYER1.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            f e e e e e e f c e c e e e e f 
+            f e e e e e e f f e e e e e e f 
+            f e c e e f f f f f f e e c e f 
+            f e c e f f f f f f f f e e e f 
+            f e e e f c f f f f f f e e e f 
+            f e e e f f f 1 1 f f f e e e f 
+            f e e e f f f f f f f f e e e f 
+            f e c e f f f f f f f f e c e f 
+            f e e e f f f f f f f f e e e f 
+            f e e e f c f f f f f f e e e f 
+            f e e e e f f f f f f e e e e f 
+            f e e c e e e e e e e e e c e f 
+            f e e e e e e e e e e e e e e f 
+            f e e e e e e e e e e c e e e f 
+            `)
         music.pewPew.play()
         pause(500)
     }
     if (controller.player2.isPressed(ControllerButton.Up)) {
-        projectile = sprites.createProjectileFromSprite(img`
+        projectile2 = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -87,6 +121,26 @@ forever(function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, PLAYER2, 0, -100)
+        PLAYER2.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            f c c c c c c f e e c c c c c f 
+            f e c c c e c f f c c c e c c f 
+            f c c c c f f f f f f c c c c f 
+            f c c c f f f f f f e f c c c f 
+            f c c c f e f f f f f f c c c f 
+            f c e c f f f 1 1 f f f c e c f 
+            f c c c f f f f f f f f c c c f 
+            e c c c f f f f f f f f c c c f 
+            f c c c f f f f f e f f c c c f 
+            f c c c f e f f f f f f e c c f 
+            f c c c c f f f f f f c c c c f 
+            f c c e c c c c c c c c c c c f 
+            f c c c c c c c e c c c e c c f 
+            f c c c c c c c c c c c c c c f 
+            `)
         music.pewPew.play()
         pause(500)
     }
@@ -109,11 +163,31 @@ forever(function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, PLAYER1, 0, 100)
+        PLAYER1.setImage(img`
+            f e e e c e e e e e e e e e e f 
+            f e e e e e e e e e e e e e e f 
+            f e c e e e e e e e e e c e e f 
+            f e e e e f f f f f f e e e e f 
+            f e e e f f f f f f c f e e e f 
+            f e e e f f f f f f f f e e e f 
+            f e c e f f f f f f f f e c e f 
+            f e e e f f f f f f f f e e e f 
+            f e e e f f f 1 1 f f f e e e f 
+            f e e e f f f f f f c f e e e f 
+            f e e e f f f f f f f f e c e f 
+            f e c e e f f f f f f e e c e f 
+            f e e e e e e f f e e e e e e f 
+            f e e e e c e c f e e e e e e f 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
         music.pewPew.play()
         pause(500)
     }
     if (controller.player2.isPressed(ControllerButton.Down)) {
-        projectile = sprites.createProjectileFromSprite(img`
+        projectile2 = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -131,6 +205,26 @@ forever(function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, PLAYER2, 0, 100)
+        PLAYER2.setImage(img`
+            f c c c c c c c c c c c c c c f 
+            f c c e c c c e c c c c c c c f 
+            f c c c c c c c c c c c e c c f 
+            f c c c c f f f f f f c c c c f 
+            f c c e f f f f f f e f c c c f 
+            f c c c f f e f f f f f c c c f 
+            f c c c f f f f f f f f c c c e 
+            f c c c f f f f f f f f c c c f 
+            f c e c f f f 1 1 f f f c e c f 
+            f c c c f f f f f f e f c c c f 
+            f c c c f e f f f f f f c c c f 
+            f c c c c f f f f f f c c c c f 
+            f c c e c c c f f c e c c c e f 
+            f c c c c c e e f c c c c c c f 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
         music.pewPew.play()
         pause(500)
     }
@@ -153,11 +247,29 @@ forever(function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, PLAYER1, -100, 0)
+        PLAYER1.setImage(img`
+            . . . . f f f f f f f f f f f f f f 
+            . . . . e e e e e e e e e e e e e e 
+            . . . . e e c e e e e c e e e c e e 
+            . . . . e e e e e e e e e e e e e e 
+            . . . . e e e f f f f f f f e e e c 
+            . . . . c e f f f f f f f f f e e e 
+            . . . . e e f f f f f f f f f e e e 
+            . f f f c f f f f 1 f f f f f e e e 
+            . f f f f f f f f 1 f f f f f e e e 
+            . . . . e e f f f f f f f f f e e e 
+            . . . . e e f f c f f f f c f e e e 
+            . . . . e e e f f f f f f f e e e e 
+            . . . . e e e e e e e e e e e c e e 
+            . . . . e e c c e e e c e e e e e e 
+            . . . . e e e e e e e e e e e e e e 
+            . . . . f f f f f f f f f f f f f f 
+            `)
         music.pewPew.play()
         pause(500)
     }
     if (controller.player2.isPressed(ControllerButton.Left)) {
-        projectile = sprites.createProjectileFromSprite(img`
+        projectile2 = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -175,6 +287,24 @@ forever(function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, PLAYER2, -100, 0)
+        PLAYER2.setImage(img`
+            . . . . f f f f f f f f f f f f f f 
+            . . . . c c c c c c c c c c c c c c 
+            . . . . c c c c c e c c c c c c c c 
+            . . . . c e c c c c c c c e c c e c 
+            . . . . c c c f f f f f f f c c c c 
+            . . . . c c f e f f f f f f f c c c 
+            . . . . e c f f f f f f e f f c c c 
+            . f f f e f f f f 1 f f f f f c e c 
+            . f f f f f f f f 1 f f f f f c c c 
+            . . . . c c f f f f f f f f f c c c 
+            . . . . c e f f e f f f f e f c c c 
+            . . . . c c c f f f f f f f c c c c 
+            . . . . c c c c c c c c c c c e c c 
+            . . . . c c c c c e c c c c c c c c 
+            . . . . c e c c c c c c c c c c c c 
+            . . . . f f f f f f f e f f f f f f 
+            `)
         music.pewPew.play()
         pause(500)
     }
@@ -197,11 +327,29 @@ forever(function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, PLAYER1, 100, 0)
+        PLAYER1.setImage(img`
+            f f f f f f f f f f f f f f . . . . 
+            e e e e e e e e e e e e e e . . . . 
+            e e e e e e c e e e c c e e . . . . 
+            e e c e e e e e e e e e e e . . . . 
+            e e e e f f f f f f f e e e . . . . 
+            e e e f c f f f f c f f e e . . . . 
+            e e e f f f f f f f f f e e . . . . 
+            e e e f f f f f 1 f f f f f f f f . 
+            e e e f f f f f 1 f f f f c f f f . 
+            e e e f f f f f f f f f e e . . . . 
+            e e e f f f f f f f f f e c . . . . 
+            c e e e f f f f f f f e e e . . . . 
+            e e e e e e e e e e e e e e . . . . 
+            e e c e e e c e e e e c e e . . . . 
+            e e e e e e e e e e e e e e . . . . 
+            f f f f f f f f f f f f f f . . . . 
+            `)
         music.pewPew.play()
         pause(500)
     }
     if (controller.player2.isPressed(ControllerButton.Right)) {
-        projectile = sprites.createProjectileFromSprite(img`
+        projectile2 = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -219,7 +367,55 @@ forever(function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, PLAYER2, 100, 0)
+        PLAYER2.setImage(img`
+            f f f f f f e f f f f f f f . . . . 
+            c c c c c c c c c c c c e c . . . . 
+            c c c c c c c c e c c c c c . . . . 
+            c c e c c c c c c c c c c c . . . . 
+            c c c c f f f f f f f c c c . . . . 
+            c c c f e f f f f e f f e c . . . . 
+            c c c f f f f f f f f f c c . . . . 
+            c c c f f f f f 1 f f f f f f f f . 
+            c e c f f f f f 1 f f f f e f f f . 
+            c c c f f e f f f f f f c e . . . . 
+            c c c f f f f f f f e f c c . . . . 
+            c c c c f f f f f f f c c c . . . . 
+            c e c c e c c c c c c c e c . . . . 
+            c c c c c c c c e c c c c c . . . . 
+            c c c c c c c c c c c c c c . . . . 
+            f f f f f f f f f f f f f f . . . . 
+            `)
         music.pewPew.play()
         pause(500)
     }
+})
+forever(function () {
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(196, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(196, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Half))
+})
+forever(function () {
+	
+})
+forever(function () {
+	
+})
+forever(function () {
+	
+})
+forever(function () {
+	
 })
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -5,17 +5,6 @@ namespace myTiles {
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "level1":
-            case "level1":return tiles.createTilemap(hex`0a0008000101010101010501010101010101010101010101010105010501010105040101010101010103010101010201010101010101010501010101010101030101010105050401050101050101010101010101`, img`
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-`, [myTiles.transparency16,sprites.castle.tileGrass1,sprites.castle.tilePath4,sprites.castle.tilePath6,sprites.castle.tilePath2,sprites.castle.tileGrass2], TileScale.Sixteen);
             case "level2":
             case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
 . . . . . . . . . . . . . . . . 
@@ -54,6 +43,17 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`0a0008000101040101010501010101010101060101010606060105010607010105040101010606010103010101010201060606010101010501010706010101060606010105060401050108070101010101010101`, img`
+. . . . . . . . . . 
+. . . . 2 . . . 2 2 
+2 . . . 2 . . . . . 
+. . . 2 2 . . . . . 
+. . . . 2 2 2 . . . 
+. . . . . 2 . . . 2 
+2 2 . . . 2 . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass1,sprites.castle.tilePath4,sprites.castle.tilePath6,sprites.castle.tilePath2,sprites.castle.tileGrass2,sprites.builtin.brick,sprites.castle.tileGrass3,sprites.castle.tilePath8], TileScale.Sixteen);
         }
         return null;
     })
